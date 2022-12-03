11ab8f6af87839ae973bf8083d4cd05a179c856b
gmgreer <beauski8@gmail.com>
2022-12-03 10:27:32 -0800

    connected backend with a Get

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,60 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './App.css'
 
+type decks = {
+  _id: string
+  title: string
+}
+
 function App() {
+  const [decks, setDecks] = useState([])
+  const [title, setTitle] = useState('')
   
+  const handleCreateDeck = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await fetch('http://localhost:5000/decks', {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    setTitle("")
+  }
+
+  useEffect(()=>{
+    const fetchDecks = async () => {
+      const response = await fetch('http://localhost:5000/decks')
+      const newDecks = await response.json()
+      setDecks(newDecks)
+    }
+    fetchDecks();
+    return ()=>{}
+  }, [])
+
 
   return (
-    <div className='bg-slate-500 h-screen'>
-      <div>Hello World</div>
+    <div className='bg-slate-500 h-screen flex flex-col justify-center items-center '>
+      <div>
+        <ul className='grid grid-cols-3 gap-5 items-center justify-center mb-5'>
+
+        {decks.map(deck => (
+          <li className='border border-black h-20 w-20 text-center bg-white shadow-md' key={deck._id}>{deck.title}</li>
+          ))}
+          </ul>
+      </div>
+      <form onSubmit={handleCreateDeck} className='flex flex-col space-y-3' >
+        <input id='deck-title' placeholder='Deck Title' type="text" 
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value)
+          }}
+        />
+        <button className='border border-black'>Create Deck</button>
+      </form>
     </div>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,23 +3,25 @@ config()
 import express from "express";
 import { Response, Request } from "express";
 import mongoose from 'mongoose'
-import DeckModel from './models/Deck'
+import Deck from './models/Deck'
+import cors from "cors"
 const PORT = 5000
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
-
+app.get('/decks', async (req: Request, res: Response) => {
+    const decks = await Deck.find()
+    res.json(decks)   
+})
 
 app.post("/decks", async (req: Request, res: Response) => {
-    const newDeck = new DeckModel({
+    const newDeck = new Deck({
         title: req.body.title
     });
     const createdDeck = await newDeck.save()
         res.json(createdDeck)
-
- 
 })
 
 mongoose.connect(process.env.MONGO_URL!)
