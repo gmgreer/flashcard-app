634dc30289062797df8379afa1355398fb9ce4bb
gmgreer <beauski8@gmail.com>
2022-12-04 09:32:10 -0800

    create read delete abstracted away

---
+++ client/src/api/getDecks.ts
@@ -0,0 +1,12 @@
+import { API_URL } from "./config"
+
+export type TDeck = {
+    _id: string
+    title: string
+  }
+
+export async function getDecks(): Promise<TDeck[]> {
+    const response = await fetch(`${API_URL}/decks`)
+      return response.json()
+      
+}
---
+++ client/src/components/Deck.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Deck = (props: Props) => {
+  return (
+    <div>Deck</div>
+  )
+}
+
+export default Deck
